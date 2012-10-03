ce459a313fa01d225f106de4a4b8c9a1b81fb1cf
Will Farrington <wfarr@github.com>
2012-10-03 15:30:37 -0700

    turn puppet fu back on

--- Puppetfile
+++ Puppetfile
@@ -11,9 +11,9 @@ mod "boxen", "0.0.1.1", :github_tarball => "boxen/puppet-boxen"
 
 # Core modules for a basic development environment.
 # You can replace some/most of those if you want, but it's not recommended.
-#%w(ruby rbenv nvm nodejs git hub inifile sudo homebrew xcode).each do |modulename|
-#  mod modulename, "0.0.1", :github_tarball => "boxen/puppet-#{modulename}"
-#end
+%w(ruby rbenv nvm nodejs git hub inifile sudo homebrew xcode).each do |modulename|
+  mod modulename, "0.0.1", :github_tarball => "boxen/puppet-#{modulename}"
+end
 
 # Add your custom modules here.
 # There are tons available at https://github.com/boxen.
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,43 +1,43 @@
-# require boxen::environment
-# require homebrew::repo
+require boxen::environment
+require homebrew::repo
 
-# Exec {
-#   group       => 'staff',
-#   logoutput   => on_failure,
-#   user        => $luser,
+Exec {
+  group       => 'staff',
+  logoutput   => on_failure,
+  user        => $luser,
 
-#   path => [
-#     "${boxen::config::home}/rbenv/shims",
-#     "${boxen::config::home}/homebrew/bin",
-#     '/usr/bin',
-#     '/bin',
-#     '/usr/sbin',
-#     '/sbin'
-#   ]
-# }
+  path => [
+    "${boxen::config::home}/rbenv/shims",
+    "${boxen::config::home}/homebrew/bin",
+    '/usr/bin',
+    '/bin',
+    '/usr/sbin',
+    '/sbin'
+  ]
+}
 
-# File {
-#   group => 'staff',
-#   owner => $luser
-# }
+File {
+  group => 'staff',
+  owner => $luser
+}
 
-# Package {
-#   provider => homebrew,
-#   require  => Class['homebrew']
-# }
+Package {
+  provider => homebrew,
+  require  => Class['homebrew']
+}
 
-# Repository {
-#   provider => git,
-#   extra    => [
-#     '--recurse-submodules'
-#   ],
-#   require  => Class['git']
-# }
+Repository {
+  provider => git,
+  extra    => [
+    '--recurse-submodules'
+  ],
+  require  => Class['git']
+}
 
-# Service {
-#   provider => ghlaunchd
-# }
+Service {
+  provider => ghlaunchd
+}
 
-# node default {
-#   include git
-# }
+node default {
+  include git
+}
