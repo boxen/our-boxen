f5a10d9d8f52306cc837fd07d2c5d78538c1a9fd
Will Farrington <wfarr@github.com>
2012-10-02 19:54:15 -0700

    update default node def

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,43 @@
+require boxen::config
+require homebrew::repo
+
+Exec {
+  group       => 'staff',
+  logoutput   => on_failure,
+  user        => $luser,
+
+  path => [
+    "${boxen::config::home}/rbenv/shims",
+    "${boxen::config::home}/homebrew/bin",
+    '/usr/bin',
+    '/bin',
+    '/usr/sbin',
+    '/sbin'
+  ]
+}
+
+File {
+  group => 'staff',
+  owner => $luser
+}
+
+Package {
+  provider => homebrew,
+  require  => Class['homebrew']
+}
+
+Repository {
+  provider => git,
+  extra    => [
+    '--recurse-submodules'
+  ],
+  require  => Class['git']
+}
+
+Service {
+  provider => ghlaunchd
+}
+
 node default {
-  notify { 'Hello, world.': }
+  include setup::environment
 }
