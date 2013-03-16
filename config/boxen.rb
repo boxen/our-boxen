4521c734cbf2173ad5af0f073dab1bb485922ff2
Grant Regimbal <regimbal.grant@gmail.com>
2013-03-15 19:52:14 -0700

    Fixed Bug
    This was causing me issues until I changed it after I found the github issue.
    It said the issue was closed but I still had the bug when I installed boxen without boxen-web.
    Previous issue: https://github.com/boxen/boxen/issues/62

--- config/boxen.rb
+++ config/boxen.rb
@@ -5,7 +5,7 @@
 # ENV['BOXEN_HOME'] = '/opt/boxen'
 
 # Change the repo boxen will use.
-# ENV['BOXEN_REPO_NAME'] = 'boxen/our-boxen'
+ ENV['BOXEN_REPO_NAME'] = 'boxen/our-boxen'
 
 # Boxen binary packaging
 # ENV["BOXEN_S3_ACCESS_KEY"] = ''
