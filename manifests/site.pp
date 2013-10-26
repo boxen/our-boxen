470b22aa5b659c328303f159732c1047c056c9b7
Paul Healey <paulhealey@brighttalk.com>
2013-10-26 23:11:23 +0100

    removed outdated ruby

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,10 +70,10 @@ node default {
 #  include nodejs::v0_10
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
-  include ruby::2_0_0
+#  include ruby::1_8_7
+#  include ruby::1_9_2
+#  include ruby::1_9_3
+#  include ruby::2_0_0
 
   # common, useful packages
   package {
