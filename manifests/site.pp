c2c68acf4b926cf7a4639d10bd6465626b6c4d77
Paul Healey <paulhealey@brighttalk.com>
2013-10-26 22:19:00 +0100

    commented out nodejs

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,10 +64,10 @@ node default {
   }
 
   # node versions
-  include nodejs::v0_4
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
+#  include nodejs::v0_4
+#  include nodejs::v0_6
+#  include nodejs::v0_8
+#  include nodejs::v0_10
 
   # default ruby versions
   include ruby::1_8_7
