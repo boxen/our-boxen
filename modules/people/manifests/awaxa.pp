127fe572cfc3a860b1f544cd5e34d6deb92f24f4
Greg Kitson <greg.kitson@gmail.com>
2014-01-07 17:57:57 -0800

    configure rbenv global ruby

--- modules/people/manifests/awaxa.pp
+++ modules/people/manifests/awaxa.pp
@@ -1,4 +1,11 @@
 class people::awaxa {
+
+  $globalruby = '2.0.0'
+
+  class { 'ruby::global':
+    version => "$globalruby"
+  }
+
   include people::awaxa::applications
   include people::awaxa::dotfiles
   include people::awaxa::gitconfig
