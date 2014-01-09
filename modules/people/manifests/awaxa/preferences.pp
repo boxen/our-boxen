3ac2546ceb472bcdf336ef8a5b5bb857ee7afdd2
Greg Kitson <greg.kitson@gmail.com>
2014-01-09 11:09:02 -0800

    remove c286265 - add key repeat delay settings

--- modules/people/manifests/awaxa/preferences.pp
+++ modules/people/manifests/awaxa/preferences.pp
@@ -14,13 +14,6 @@ class people::awaxa::preferences {
   include osx::no_network_dsstores
   include osx::software_update
 
-  class { 'osx::global::key_repeat_delay':
-    delay => 15,
-  }
-  class { 'osx::global::key_repeat_rate':
-    rate => 2,
-  }
-
   boxen::osx_defaults {
     'Globally Disable Autocorrect':
       user   => "${::boxen_user}",
