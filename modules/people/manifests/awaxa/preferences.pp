0d38dfef7309a34523ae249651bbc8778fb91e20
Greg Kitson <greg.kitson@gmail.com>
2014-02-01 10:01:48 -0800

    configure screenshot save directory

--- modules/people/manifests/awaxa/preferences.pp
+++ modules/people/manifests/awaxa/preferences.pp
@@ -1,5 +1,7 @@
 class people::awaxa::preferences {
 
+  $home = "/Users/${::boxen_user}"
+
   #osx::recovery_message { "If found, please email greg.kitson@gmail.com": }
 
   include osx::global::disable_remote_control_ir_receiver
@@ -20,4 +22,16 @@ class people::awaxa::preferences {
       domain => 'com.apple.dashboard',
       value  => '1',
   }
+
+  file { "$home/Screenshots":
+    ensure => directory,
+  }
+  boxen::osx_defaults { 'Set Screenshot Directory':
+      user    => "${::boxen_user}",
+      key     => 'location',
+      domain  => 'com.apple.screencapture',
+      value   => "$home/Screenshots",
+      require => File["$home/Screenshots"],
+  }
+
 }
