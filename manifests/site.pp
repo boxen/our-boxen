701bce247bd5680b00a135913fc630e95d40dfe3
Jason Yeo <jasonyeo88@gmail.com>
2014-09-18 01:22:08 +0800

    Removed rubies

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,9 +70,6 @@ node default {
 
   # default ruby versions
   ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
