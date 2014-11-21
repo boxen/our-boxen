39dbf804586b04a1c6db4f539f31d95d65efc207
steve450 <steve@cylentsystems.com>
2014-11-21 08:35:24 -0500

    Set my OS X defaults

--- modules/people/manifests/steve450.pp
+++ modules/people/manifests/steve450.pp
@@ -38,14 +38,30 @@ class people::steve450 {
     require => [Repository[$dotfiles],File[$env]]
   }
 
-  boxen::osx_defaults { 'show all files in Finder':
-    ensure => present,
-    domain => 'com.apple.Finder',
-    key    => 'AppleShowAllFiles',
-    value  => 'true',
-    type   => 'bool',
-    user   => $::boxen_user
+  # OSX Defaults
+  include osx::global::enable_keyboard_control_access
+  include osx::global::disable_autocorrect
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_hidden_files
+  include osx::finder::show_all_filename_extensions
+  include osx::safari::enable_developer_mode
+
+  class { 'osx::global::natural_mouse_scrolling':
+    enabled => false
   }
+
+  include osx::dock::icon_size
+
+  class { 'osx::dock::hot_corners':
+    top_left => "Launchpad",
+    bottom_left => "Mission Control",
+    top_right => "Start Screen Saver",
+    bottom_right => "Application Windows"
+  }
+
+  # End OSX Defaults
 
   package {
     [
