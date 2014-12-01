7ac8dae703b86b3de2e5a5b145ecf43f137532c4
steve450 <steve@cylentsystems.com>
2014-12-01 10:46:42 -0500

    Install vagrant vmware plugin and license

---
+++ modules/cylent/manifests/vagrant/vagrant_vmware.pp
@@ -0,0 +1,23 @@
+class cylent::vagrant::vagrant_vmware {
+  notify { 'class cylent::vagrant::vagrant_vmware declared':}
+
+  $repo_dir = "/Users/${::boxen_user}/cylent"
+  $license_file = "${repo_dir}/licenses/vagrant-vmware-fusion/${github_login}/license.lic"
+
+  $install_cmd = "vagrant plugin install vagrant-vmware-fusion"
+  $install_license_cmd = "vagrant plugin license vagrant-vmware-fusion ${license_file}"
+
+  exec { $install_cmd:
+    command   => $install_cmd,
+    unless    => "vagrant plugin list | grep vagrant-vmware-fusion",
+    require   => Class['vagrant'],
+  }
+
+  exec { $install_license_cmd:
+    command     => $install_license_cmd,
+    onlyif      => "test -e $license_file",
+    refreshonly => true,
+    require     => Repository["${repo_dir}/licenses"],
+    subscribe   => Exec[$install_cmd],
+  }
+}
--- modules/projects/manifests/portcullis.pp
+++ modules/projects/manifests/portcullis.pp
@@ -3,6 +3,8 @@ class projects::portcullis {
   $home       = "/Users/${::boxen_user}"
   $repo_dir   = "${home}/cylent"
 
+  include cylent::vagrant::vagrant_vmware
+
   repository { "${repo_dir}/vagrantfiles":
     source => 'cylentsystems/vagrantfiles',
     require => File[$repo_dir]
@@ -15,6 +17,11 @@ class projects::portcullis {
 
   repository { "${repo_dir}/portcullis":
     source => 'cylentsystems/portcullis',
+    require => File[$repo_dir]
+  }
+
+  repository { "${repo_dir}/licenses":
+    source => 'cylentsystems/licenses',
     require => File[$repo_dir]
   }
 }
