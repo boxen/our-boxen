8316014e817cf7b30fc6da064d0b8325bf6eae2d
steve450 <steve@cylentsystems.com>
2014-12-01 12:00:18 -0500

    Add basic cylent facts

--- modules/cylent/lib/facter/cylent.rb
+++ modules/cylent/lib/facter/cylent.rb
@@ -1,3 +1,10 @@
 require 'facter'
 
-Facter.add(:cylent_repo_dir) { setcode "echo $HOME/cylent" }
+facts = {}
+
+facts["home"] = "#{ENV['HOME']}"
+facts["cylent_repo_dir"] = "#{ENV['HOME']}/cylent"
+facts["cylent_dotfiles"] = "#{ENV['HOME']}/cylent/dotfiles"
+facts["cylent_env"] = "#{ENV['HOME']}/.env"
+
+facts.each { |k, v| Facter.add(k) { setcode { v } } }
