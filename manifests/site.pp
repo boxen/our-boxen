952c3d4e91b7a05b60d2fea40f613f7ec19adf4c
ryan00 <ryan@cylentsystems.com>
2014-12-19 16:32:20 -0600

    moved repository to dev_environment because the order was screwing up fresh installs

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,17 +34,6 @@ Package {
   require  => Class['homebrew']
 }
 
-Repository {
-  provider => git,
-  extra    => [
-    '--recurse-submodules'
-  ],
-  require  => File["${boxen::config::bindir}/boxen-git-credential"],
-  config   => {
-    'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
-  }
-}
-
 Service {
   provider => ghlaunchd
 }
--- modules/cylent/manifests/dev_environment.pp
+++ modules/cylent/manifests/dev_environment.pp
@@ -1,6 +1,17 @@
 class cylent::dev_environment {
   notify { 'class cylent::dev_envronment declared': }
 
+  Repository {
+    provider => git,
+    extra    => [
+      '--recurse-submodules'
+    ],
+    require  => File["${boxen::config::bindir}/boxen-git-credential"],
+    config   => {
+      'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
+    }
+  }
+
   include cylent::apps::default_apps
   include cylent::vagrant::vagrant_vmware
 
