32ba4e7ca44379b26bcc764625ed3c3448169ffa
Steve Carlucci <scarlucci@sonatype.com>
2015-01-13 15:37:18 -0500

    Endpoint project for boxen

---
+++ modules/projects/manifests/endpoint.pp
@@ -0,0 +1,22 @@
+class projects::endpoint {
+
+  repository { "${cylent_repo_dir}/portcullis":
+    source => 'cylentsystems/portcullis',
+    require => File[$cylent_repo_dir]
+  }
+
+  repository { "${cylent_repo_dir}/kimya":
+    source => 'cylentsystems/kimya',
+    require => File[$cylent_repo_dir]
+  }
+
+  repository { "${cylent_repo_dir}/trebuchet-x86":
+    source => 'cylentsystems/trebuchet-x86',
+    require => File[$cylent_repo_dir]
+  }
+
+  repository { "${cylent_repo_dir}/endpoint-distribution":
+    source => 'cylentsystems/endpoint-distribution',
+    require => File[$cylent_repo_dir]
+  }
+}
