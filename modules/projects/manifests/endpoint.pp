88846ab428e5ab589886162079b311b3600d9823
steve450 <steve@cylentsystems.com>
2015-01-20 16:20:41 -0500

    Remove obsolete repositories

--- modules/projects/manifests/endpoint.pp
+++ modules/projects/manifests/endpoint.pp
@@ -1,27 +1,12 @@
 class projects::endpoint {
 
-  repository { "${cylent_repo_dir}/portcullis":
-    source => 'cylentsystems/portcullis',
-    require => File[$cylent_repo_dir]
-  }
-
   repository { "${cylent_repo_dir}/kimya":
     source => 'cylentsystems/kimya',
     require => File[$cylent_repo_dir]
   }
 
-  repository { "${cylent_repo_dir}/kale":
-    source => 'cylentsystems/kale',
-    require => File[$cylent_repo_dir]
-  }
-
   repository { "${cylent_repo_dir}/trebuchet-x86":
     source => 'cylentsystems/trebuchet-x86',
-    require => File[$cylent_repo_dir]
-  }
-
-  repository { "${cylent_repo_dir}/endpoint-distribution":
-    source => 'cylentsystems/endpoint-distribution',
     require => File[$cylent_repo_dir]
   }
 }
