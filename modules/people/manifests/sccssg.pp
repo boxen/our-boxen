18592ca0d0ddbd55d1141f5ae89c9ef569678479
Sunny Gleason <sunny.gleason@cylentsystems.com>
2015-02-06 09:51:51 -0500

    add mr. sg

---
+++ modules/people/manifests/sccssg.pp
@@ -0,0 +1,60 @@
+class people::sccssg {
+  ##### Custom Apps #####
+  include spectacle
+  include virtualbox
+  include docker
+  include redis
+  include homebrew
+  include stdlib
+  include quicksilver
+#  include cylent::apps::ansible
+#  notify {'awscli':}
+  include sysctl
+  include postgresql
+  include cylent::dev_environment
+  # TODO: include menumeters
+
+  ###### Environment Settings ##########
+  include osx::dock::autohide
+  include osx::dock::dim_hidden_apps
+  include osx::finder::show_all_on_desktop
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_hidden_files
+
+  class { 'osx::global::natural_mouse_scrolling':
+    enabled => true
+  }
+
+  class { 'osx::dock::hot_corners':
+    top_right => "Put Display to Sleep",
+    top_left  => "Disable Screen Saver",
+    bottom_right => "Put Display to Sleep",
+    bottom_left  => "Put Display to Sleep" 
+  }
+
+  include cylent::osx::dock::minimize_to_application
+
+  ###### Set up oh-my-zsh environment ######
+  repository {"${cylent_repo_dir}/oh-my-zsh":
+    source => 'robbyrussell/oh-my-zsh',
+    require => File[$cylent_repo_dir]
+  }
+
+  file {"${home}/.zshrc":
+    ensure  => link,
+    target => "${cylent_dotfiles}/zshrc",
+    require => Repository["${cylent_repo_dir}/oh-my-zsh"]
+  }
+
+  file {"${cylent_env}/zsh":
+    ensure => link,
+    target => "${cylent_dotfiles}/zsh",
+    require => [Repository[$cylent_dotfiles],File[$cylent_env]]
+  }
+  ->
+  exec { "chsh -s /opt/boxen/homebrew/bin/zsh":
+    user => root,
+    path => ["/usr/bin","/bin"],
+    onlyif => "bash -c test `dscl . -read /Users/${USER} UserShell | cut -d: -f2 | tr -d ' '` = /opt/boxen/homebrew/bin/zsh"
+  }
+}
