0aaf5f37448401669f777fb75aabbdf5813ed510
Sunny Gleason <sunny.gleason@cylentsystems.com>
2015-02-06 10:04:07 -0500

    add portal repos

--- modules/projects/manifests/portal.pp
+++ modules/projects/manifests/portal.pp
@@ -6,4 +6,14 @@ class projects::portal {
   include stdlib
   include sysctl
   include postgresql
+
+  repository { "${cylent_repo_dir}/angalia-web":
+    source => 'cylentsystems/angalia-web',
+    require => File[$cylent_repo_dir]
+  }
+
+  repository { "${cylent_repo_dir}/angalia-api":
+    source => 'cylentsystems/angalia-api',
+    require => File[$cylent_repo_dir]
+  }
 }
