630cbbfb6ffc002509bd00d5d422619144e3956f
Mike Splain <mike.splain@cylentsystems.com>
2015-05-14 10:09:22 -0400

    Give Mike some stuff

--- modules/people/manifests/captmikeduffy.pp
+++ modules/people/manifests/captmikeduffy.pp
@@ -1,20 +1,28 @@
-class people::captmikeduffy {
+class people::jockod {
   include cylent::dev_environment
+  include python::virtualenvwrapper
+
+  include projects::endpoint
+  include projects::portal
+  include caffeine
+
 
   ###### Environment Settings ##########
-  #include osx::dock::autohide
-  #include osx::dock::dim_hidden_apps
-  #include osx::finder::show_all_on_desktop
+#  include osx::dock::autohide
+#  include osx::dock::dim_hidden_apps
+#  include osx::finder::show_all_on_desktop
   include osx::finder::empty_trash_securely
-  #include osx::finder::show_hidden_files
+  include osx::global::tap_to_click
+
+#  include osx::finder::show_hidden_files
 
 
-  #class { 'osx::dock::hot_corners':
-  #  top_right => 'Application Windows',
-  #  top_left  => 'Desktop',
-  #  bottom_right => 'Start Screen Saver',
-  #  bottom_left => 'Dashboard'
-  #}
+#  class { 'osx::dock::hot_corners':
+#    top_right => 'Application Windows',
+#    top_left  => 'Desktop',
+#    bottom_right => 'Start Screen Saver',
+#    bottom_left => 'Dashboard'
+#  }
 
   include cylent::osx::dock::minimize_to_application
 
