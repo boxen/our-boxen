9588f1639fc41d64d9842381a92e73e454e5ee57
J$ <jmonette@homeaway.com>
2015-06-08 16:52:18 -0500

    Add chrome

--- modules/people/manifests/jmonette.pp
+++ modules/people/manifests/jmonette.pp
@@ -2,5 +2,5 @@ class people::jmonette {
   include brewcask
   
   package { 'skype': provider => 'brewcask' }
-  packave { 'chrome': provider => 'brewcask' }
+  package { 'chrome': provider => 'brewcask' }
 }
