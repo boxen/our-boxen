e671ed99e9e843f4cee9471b217301a95e17bc1d
J$ <jmonette@homeaway.com>
2015-06-08 16:30:25 -0500

    Let's get skype running first

--- modules/people/manifests/jmonette.pp
+++ modules/people/manifests/jmonette.pp
@@ -1,13 +1,6 @@
 class people::jmonette {
 
-  package {'emacs': provider => brewcask'}
-  package {'iterm2::stable': provider => 'brewcask'}
-  package {'chrome': provider => 'brewcask'}
   package {'skype': provider => 'brewcask'}
 
-  include brewcask
-  include emacs
-  include iterm2::stable
-  include chrome
   include skype
 }
