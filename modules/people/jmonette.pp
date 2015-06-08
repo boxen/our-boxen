5143b2d0790176413e3b395890f1a073d29b69aa
J$ <jmonette@homeaway.com>
2015-06-08 15:46:27 -0500

    Add skype to jmonette.pp

--- modules/people/jmonette.pp
+++ modules/people/jmonette.pp
@@ -1,6 +1,11 @@
-class people::jjmonette {
+class people::jmonette {
   include emacs   # requires emacs module in Puppetfile
   include iterm2:stable
   include brewcask
   include chrome
+  include skype
+
+  package {
+    'skype': provier => 'brewcask'
+  }
 }
