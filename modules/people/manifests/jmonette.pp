5249090ece0c85f73870a2fb3df42685a3888744
J$ <jmonette@homeaway.com>
2015-06-08 16:23:58 -0500

    reorg jmonette.pp

--- modules/people/manifests/jmonette.pp
+++ modules/people/manifests/jmonette.pp
@@ -1,12 +1,13 @@
 class people::jmonette {
+
+  package {'emacs': provider => brewcask'}
+  package {'iterm2::stable': provider => 'brewcask'}
+  package {'chrome': provider => 'brewcask'}
+  package {'skype': 'provider => 'brewcask'}
+
   include brewcask
   include emacs
   include iterm2::stable
   include chrome
   include skype
-
-  package { 'skype': provider => 'brewcask' }
-  package { 'emacs': provider => 'brewcask' }
-  package { 'iterm2': provider => 'brewcask' }
-  package { 'chrome': provider => 'brewcask' }
 }
