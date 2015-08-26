262638888c3eac2f211bcea17d278cbed4f582b1
ryan00 <ryan@cylentsystems.com>
2015-08-26 17:25:20 -0500

    fixed error where environment was not set correctly for cylent.rb facts to be set correctly

--- modules/cylent/lib/facter/cylent.rb
+++ modules/cylent/lib/facter/cylent.rb
@@ -1,10 +1,14 @@
 require 'facter'
+require "boxen/config"
+
+config      = Boxen::Config.load
 
 facts = {}
 
-facts["home"] = "#{ENV['HOME']}"
-facts["cylent_repo_dir"] = "#{ENV['HOME']}/cylent"
-facts["cylent_dotfiles"] = "#{ENV['HOME']}/cylent/dotfiles"
-facts["cylent_env"] = "#{ENV['HOME']}/.env"
+
+facts["home"] = "/Users/#{config.user}"
+facts["cylent_repo_dir"] = "/Users/#{config.user}/cylent"
+facts["cylent_dotfiles"] = "/Users/#{config.user}/cylent/dotfiles"
+facts["cylent_env"] = "/Users/#{config.user}/.env"
 
 facts.each { |k, v| Facter.add(k) { setcode { v } } }
