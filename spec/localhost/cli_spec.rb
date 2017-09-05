aed286758bba18bf083321c6895cc91625013868
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-09-05 12:45:27 +1000

    Coverage for command output for `--help`

--- spec/localhost/cli_spec.rb
+++ spec/localhost/cli_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe 'script/nuke' do
+  describe command('/opt/boxen/repo/script/nuke --help') do
+    its(:content) { should include '--all' }
+    its(:content) { should include '--force' }
+    its(:content) { should include '--opt' }
+    its(:content) { should include '--services' }
+    its(:content) { should include '--receipts' }
+    its(:content) { should include '--gitconfig' }
+    its(:content) { should include '--homebrew-config' }
+  end
+
   describe '--homebrew-config' do
     it 'cleans out /etc/boxen' do
       creation_command_exit_status = command('touch /etc/boxen/service.config').exit_status
