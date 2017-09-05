390ec631da69545107ef19a2337031fbc48775bd
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-09-05 13:12:50 +1000

    Use rspec expect syntax

--- spec/localhost/cli_spec.rb
+++ spec/localhost/cli_spec.rb
@@ -17,9 +17,9 @@ describe 'script/nuke' do
       cleanup_command_exit_status  = command('/opt/boxen/repo/script/nuke --force --homebrew-config').exit_status
       file_count_stdout  = command('find /etc/boxen -type f | wc -l').stdout.strip
 
-      creation_command_exit_status.should eq(0)
-      cleanup_command_exit_status.should eq(0)
-      file_count_stdout.should eq("0")
+      expect(creation_command_exit_status).to eq(0)
+      expect(cleanup_command_exit_status).to eq(0)
+      expect(file_count_stdout).to eq("0")
     end
   end
 end
